20cefb9c042a69c3fd371a27a8e04dc8fcadbb96
holleAllen <40431813+holleAllen@users.noreply.github.com>
2018-06-29 12:34:23 +0800

    Add files via upload

--- zhigong.cpp
+++ zhigong.cpp
@@ -1,187 +1,327 @@
-#include<stdlib.h>
+#include<stdio.h>
 #include<conio.h>
+#include<string.h>
 #include<iostream>
 using namespace std;
-void read();
-void write(struct stu gx[],int out);
+struct member{
+	char num[20];
+	char name[40];
+    float gg;
+    float xj;
+    float jt;
+    float jx;
+    float yf;
+    float gere;
+    float sf;
+}zggz[100];
+void read();     
+void write();
 void find();
+void find_num();
 void modify();
 void del();
 void add();
-void grsde();
-struct stu{
-	char num[20];
-	char name[40];
-    float gangwei;
-    float xinji;
-    float jintie;
-    float jixiao;
-    float yingfa;
-    float gere;
-    float shifa;
-}gx[100],*p;
+void list();
+void grsde();   //声明函数
 int n=0;
-void read()          //读取职工工资数据
+void read()
 {
 	FILE *fp;
 	int i;
-    p=gx;
-	if((fp=fopen("gz5","rb"))==NULL)
+    
+	if((fp=fopen("gz10.dat","rb"))==NULL)
 	{
-		cout<<"Cannot open file strike any key exit!";
+		printf("Cannot open file strike any key exit!");
 		getch();
 		exit(-1);
 	}
-	for(i=0;i<52;i++,p++)
+	for(i=0;i<3;i++)
 	{
-		fread(p,sizeof(struct stu),52,fp);
+		fread(&zggz[3],sizeof(struct member),3,fp);
 		n++ ; 
-		cout<<" "<<p->num<<" "<<p->name<<" "<<p->gangwei<<" "
+		/*cout<<" "<<p->num<<" "<<p->name<<" "<<p->gangwei<<" "
 			     <<p->xinji<<" "<<p->jintie<<" "<<p->jixiao<<" "
 				 <<p->yingfa<<" "<<p->gere<<" "<<p->shifa<<endl;
 	}
 	cout<<"               "<<"................共有"<<n<<"个人"<<endl;
+	printf("            .......共有\n");*/  //这里有错！
+	}
 	fclose(fp);
-
 }
-/*void write()
+void write()
 {
-	FILE *fp;
 	int i;
-	if((fp=fopen("gz5","wb"))==NULL)
+	FILE*fp;
+	if((fp=fopen("gz10.dat","wb"))==NULL)
 	{
-		cout<<"Cannot open file strike any key exit!";
+		printf("Cannot open file strike any key exit!");
 		getch();
 		exit(-1);
 	}
-	for(i=0;i<n;i++)
+	for(i=0;i<3;i++)
 	{
-		if(fwrite(p,sizeof(struct stu),1,fp)!=1)
-		{	cout<<"文件读写错误";
+		if(fwrite(&zggz[i],sizeof(struct member),3,fp)!=1)
+		{	printf("文件读写错误\n");
 		    fclose(fp);
 		}
 	}
-}*/
-void write(struct stu gx[],int out)
-{
-	FILE *fp;
-	int i;
-	if ((fp = fopen("gz5", "w")) == NULL)
-	{
-		printf("文件打开失败");
-     	return;
-	}
-	if (out==0)
-		{         printf("没有联系人可读");    }       //写入数据       
-	for (i=0;i<out;i++)
-	{      fprintf(fp, "%d\t", gx[i].num);
-	       fprintf(fp, "%d\t", gx[i].name); 
-		   fprintf(fp, "%d\t", gx[i].gangwei);
-		   fprintf(fp, "%d\t", gx[i].xinji);
-		   fprintf(fp, "%d\n", gx[i].jintie);
-		   fprintf(fp, "%d\n", gx[i].jixiao);
-		   fprintf(fp, "%d\n", gx[i].yingfa);
-		   fprintf(fp, "%d\n", gx[i].gere);
-		   fprintf(fp, "%d\n", gx[i].shifa);
-	}
-	       fclose(fp);
 }
-
 void find()
 {
-	int i;
-	int flag=0;
-	int j;
-	int number;
-	cout<<"请输入要查询员工的工号\n";
-	cin>>number;
-	for(i=0;i<n;i++)
-	{	if(gx[i].num==number){
-			flag=1;
+	int t,kongzi;
+	system("cls");
+	do
+	{
+		printf("\n按1则按工号查询\n按2 返回\n");
+		scanf("%d",&t);
+		if(t>=1&&t<=3)
+		{
+			kongzi=1;
+			break;
+		}
+		else
+		{
+			kongzi=0;
+			printf("输入错误");
+			find();	
+		}
+	}
+	while(kongzi=2);
+	while(kongzi=1)
+	{
+		switch(t)
+		{
+		case 1:printf("按工号查询\n");
+			find_num();
+			break;
+		case 2:
+		//	main();
+			break;
+		default:break;
+			
+		}
+	}
+}
+void find_num()
+{
+	char num1[20];
+	int i,t;
+	printf("请输入要查找的职工号\n");
+	scanf("%s",&num1);
+	for(i=0;i<3;i++)
+	{
+		if(strcmp(num1,zggz[i].num)==0)
+		{
+			printf("工号  姓名  岗位工资  薪级工资  职位津贴  绩效工资  应发工资  所得税  实发工资\n");
+			printf("\n %s%s %d%d%d%d%d%d%d",zggz[i].num,zggz[i].name,zggz[i].gg,zggz[i].xj,
+				zggz[i].jt,zggz[i].xj,zggz[i].yf,zggz[i].gere,zggz[i].sf);
+			break;
+		}
+		else
+		{
+			printf("对不起，查无此人\n");
+			printf("\n");
+			printf("继续查询职工请按1，返回主菜单请按2\n");
+			scanf("%d",&t);
+			switch(t)
+			{
+			case 1:find_num();
+				break;
+			case 2:
+				find();
+				break;
+			default:break;
+			}
+		}
+	}
+
+}
+void add()
+{
+	FILE*fp; 
+    //int n1; 
+    printf("原来的职工信息:\n");
+    printf("\n"); 
+    fp=fopen("gz10.dat","a"); 
+	printf("\n 请输入新增加职工的信息:\n"); 
+
+    printf("请输入职工号:  "); 
+    scanf("%s",&zggz[n+1].num); 
+    printf("\n"); 
+    printf("请输入姓名:  "); 
+    scanf("%s",&zggz[n+1].name); 
+
+	getchar(); 
+
+	printf("请输入岗位工资:  "); 
+    scanf("\t%d",&zggz[n+1].gg);
+	printf("请输入薪级工资:  "); 
+	scanf("%d",&zggz[n+1].xj);
+	printf("请输入津贴:  "); 
+	scanf("%d",&zggz[n+1].jt);
+	printf("请输入绩效工资:  "); 
+	scanf("\t%d",&zggz[n+1].jx);
+	printf("请输入应发工资:  "); 
+	scanf("%d",&zggz[n+1].yf);
+	printf("请输入个人所得税:  "); 
+	scanf("%d",&zggz[n+1].gere);  
+	printf("请输入实发工资:  "); 
+	scanf("%d",&zggz[n+1].sf); 
+	
+	printf("\n"); 
+	printf("\n添加成功\n");  
+	printf("\n增加后的所有职工信息:\n"); 
+	printf("\n"); 
+	write(); //直接保存
+	list();//显示添加后的信息 
+
+    fclose(fp); 
+}
+void del()
+{
+	int put=n;
+	int i,j,k,t,kongzi;
+	char name[20];
+	printf("按姓名删除\n");
+	scanf("%s",name);
+	for(kongzi=1,i=0;kongzi&&i<put;i++)
+	{
+		if(strcmp(zggz[i].name,name)==0)
+		{
+			printf("此人原始记录为：\n");
+			printf("工号  姓名  岗位工资  薪级工资  职位津贴  绩效工资  应发工资  所得税  实发工资\n");
+			printf("\n %s%s %d%d%d%d%d%d%d",zggz[i].num,zggz[i].name,zggz[i].gg,zggz[i].xj,
+				zggz[i].jt,zggz[i].xj,zggz[i].yf,zggz[i].gere,zggz[i].sf);
+			printf("确认删除请按1，返回请按0\n");
+			scanf("%d",&k);
+			    if(k==1)
+				{
+					for(j=i;j<n-1;j++)
+					{
+						strcpy(zggz[j].name,zggz[j+1].name);
+						       zggz[j].num,zggz[j+1].num;
+							   zggz[j].gg,zggz[j+1].gg;
+							   zggz[j].xj,zggz[j+1].xj;
+							   zggz[j].jt,zggz[j+1].jt;
+							   zggz[j].jx,zggz[j+1].jx;
+							   zggz[j].yf,zggz[j+1].yf;
+							   zggz[j].gere,zggz[j+1].gere;
+							   zggz[j].sf,zggz[j+1].sf;
+					}
+					kongzi=0;
+				}
+		}
+	}
+	if(!kongzi)
+		n=n-1;
+	else
+	{
+		printf("查无此人...\n");
+	    write();           //保存信息
+		printf("继续删除请按1，不再删除请按0\n");
+		scanf("%d",&t);
+		switch(t)
+		{
+		case 1:del();
+			break;
+		case 0:
+			break;
+		default:
 			break;
 		}
 	}
-	if(flag)
-	{
-		cout<<"工号 姓名 岗位工资 薪级工资 职位津贴 绩效 应发 个人所得税 实发\n";
-		cout<<gx[i].num<<endl;
-		cout<<gx[i].name<<endl;
-	}
-	for(j=i+1;j<n;j++)
-	{
-		if(gx[j].num==number)
-		{
-			cout<<gx[i].num<<endl;
-		    cout<<gx[i].name<<endl;
-		}
-	}
-	else
-	{
-		cout<<"没有找到该工号的员工\n";
-	}
-	system("pause");
 }
 void modify()
-{}
-void del()
-{}
-void add()
-{}
+{
+	int i;
+	char num[20];
+	printf("请输入要修改的职工工号：\n");
+	scanf("%s",num);
+	for(i=0;i<n;i++)
+	{
+		if(strcmp(zggz[i].num,num)==0)
+		{
+			printf("修改后的姓名为：\n");
+			scanf("%s",&zggz[i].name);
+			printf("修改后的岗位工资为：\n");
+			scanf("%d",&zggz[i].gg);
+			printf("修改后的薪级工资为：\n");
+			scanf("%d",&zggz[i].xj);
+			printf("修改后的职务津贴为：\n");
+			scanf("%d",&zggz[i].jt);
+			printf("修改后的效绩工资为：\n");
+			scanf("%d",&zggz[i].xj);
+			printf("修改后的应发工资为：\n");
+			scanf("%d",&zggz[i].yf);
+		}
+	}
+	write();         //保存信息
+}
+void list()
+{
+	int i;
+	system("cls");
+	printf("工号  姓名  岗位工资  薪级工资  职位津贴  绩效工资  应发工资  所得税  实发工资\n");
+	for(i=0;i<n;i++)
+	{
+		printf("\n %s%s %d%d%d%d%d%d%d",zggz[i].num,zggz[i].name,zggz[i].gg,zggz[i].xj,
+				zggz[i].jt,zggz[i].xj,zggz[i].yf,zggz[i].gere,zggz[i].sf);
+	}
+}
 void grsde()
 {}
 int main()
 {
-	int out;
-	
-	cout<<"                 员工管理系统                 \n";
+	int count;
+	printf("\n\n");
+	printf("      ###  欢迎使用广西民族大学软件与信息安全学院职工工资管理系统！  ###\n\n");
+	printf("\n");
 	do{
-		system("cls");
-		cout<<"\n\n\n";
-		cout<<"               ******              功能菜单选择           ******\n\n";
-		cout<<"               ******           1、显示所有员工信息       ******\n";
-		cout<<"               ******           2、保存员工信息           ******\n";
-		cout<<"               ******           3、查找员工信息           ******\n";
-		cout<<"               ******           4、修改员工信息           ******\n";
-		cout<<"               ******           5、删除员工信息           ******\n";
-		cout<<"               ******           6、添加员工信息           ******\n";
-		cout<<"               ******           7、查看个人所得           ******\n";
-		cout<<"               ******           0、退出程序               ******\n";
-		cout<<"\n\n\n";
-		cout<<"           请输入你的选择：\n";
-		cin>>out;
-		switch(out)
+		//system("cls");
+	    printf("          请选择<1 - 7>:\n");
+		printf("      ===================================================================\n");
+    	printf("      |            1、查询职工工资信息                                  |\n");
+	    printf("      |            2、修改职工工资信息                                  |\n");
+    	printf("      |            3、添加职工工资信息                                  |\n");
+	    printf("      |            4、删除职工工资信息                                  |\n");
+	    printf("      |            5、保存数据到文件                                    |\n");
+	    printf("      |            6、浏览职工记录                                      |\n");
+	    printf("      |            7、退出系统                                          |\n");
+		printf("      ===================================================================");
+		printf("\n");
+	    printf("      请输入您的选择：\n");
+	    scanf("%d",&count);
+	    switch(count)
 		{
         case 1:
-            read();
+            find();
 			break;
 		case 2:
-			write(gx,out);
-			break;
-		case 3:
-			find();
-			break;
-		case 4:
 			modify();
 			break;
-		case 5:
-			del();
-			break;
-		case 6:
+		case 3:
 			add();
 			break;
-		case 7:
-			grsde();
+		case 4:
+			del();
 			break;
-		case 0:
-			cout<<"\n谢谢使用！\n";
+		case 5:
+			write();
+			break;
+		case 6:
+			list();
+			break;
+		case 7:
+			printf("\n谢谢使用！\n");
 			return 0;
+
 		default:
-			cout<<"\n输入有误，请重新输入：";
-			cin>>out;
+			printf("\n输入有误，请重新输入：");
+			scanf("%d",&count);
 			break;
 
 		}
-	}while(out!=-1);
+	}while(count!=-1);
 	
 	/*cout<<"工号"<<" "<<"姓名"<<" "<<"岗位工资"<<" "
 		<<"薪级工资"<<" "<<"职务津贴"<<" "<<"绩效工资"<<" "
@@ -190,3 +330,4 @@ int main()
 	write();*/
 	return 0;
 }
+
